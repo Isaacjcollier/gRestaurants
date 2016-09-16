89b133ef56a9c0860239c26170dfc23edcd088ed
Phil Benz <philbenz@gmail.com>
2016-09-16 14:40:19 -0600

    updated auth

--- src/client/js/new_user.js
+++ src/client/js/new_user.js
@@ -1,14 +1,34 @@
 (function() {
 
   'use strict';
-  console.log('new_user werkin');
+  console.log('new_user sign up!!');
 
-  $('#modal-sign-in').on('click', (event) => {
+  $('#sign-up-modal').on('click', (event) => {
     event.preventDefault();
-    const userName = $('#input-username').val();
-    const password = $('#input-password').val();
-    console.log(userName);
-    console.log(password);
+
+    const userPayload = {
+      firstName: $('#input-firstName').val(),
+      lastName: $('#input-lastName').val(),
+      userName: $('#input-username').val(),
+      password: $('#input-new-password').val(),
+      email: $('#input-user-email').val()
+    };
+
+    //PUT request with payload for server
+    $.ajax({
+      type: 'POST',
+      url: '/api/v1/users/new',
+      data: userPayload
+    })
+    .done((data) => {
+      //toggle modal off when complete
+      $('#myModal-edit').modal('toggle');
+      //reload page to show updates
+      location.reload();
+    })
+    .fail((error) => {
+      console.log(error);
+    });
   });
 
 })();
--- src/server/routes/users.js
+++ src/server/routes/users.js
@@ -2,11 +2,50 @@ const express = require('express');
 const router = express.Router();
 const knex = require('../db/knex');
 const genres = require('../genres.json');
+const bcrypt = require('bcrypt');
 
 function users() { return knex('users'); }
+function joinUserPwds() { return knex('join_users_passwords'); }
+function passwords() { return knex('passwords'); }
 
 router.get('/', function (req, res, next) {
   res.render('restaurant_review');
+});
+
+router.post('/new', function (req, res, next) {
+  // Hash the password with the salt
+  var hash = bcrypt.hashSync(req.body.password);
+
+  knex('users')
+  .insert({
+    first_name: req.body.firstName,
+    last_name: req.body.lastName,
+    email: req.body.email,
+    username: req.body.userName
+  })
+  .then((results) => {
+    console.log('inserting into passwords');
+    knex('passwords')
+    .returning('id')
+    .insert({
+      password: hash
+    })
+    .then((results) => {
+      console.log(results);
+      if (results) {
+        res.redirect('/api/v1/restaurants');
+      } else {
+        res.status(500).send({
+          status: 'error',
+          message: 'error'
+        });
+      }
+    })
+  .catch((err) => {
+      console.log(err);
+      res.status(500);
+    });
+  });
 });
 
 router.post('/', (req, res, next) => {
