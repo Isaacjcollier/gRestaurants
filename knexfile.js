707cdf44523c8e4e8ed76cb4889a42498d0bdaa6
Isaacjcollier <isaacjcollier@gmail.com>
2016-09-19 08:29:27 -0600

    added port to knex.js

--- knexfile.js
+++ knexfile.js
@@ -4,6 +4,7 @@ module.exports = {
   development: {
     client: 'postgresql',
     connection: `postgres://mrntsdnvyezner:wqdOLzVBAZPV34RVZ3gt7AZkgF@ec2-54-243-54-21.compute-1.amazonaws.com:5432/d9uf1c8i3t74bt`,
+    port:3000,
     migrations: {
       directory: __dirname + '/src/server/db/migrations'
     },
